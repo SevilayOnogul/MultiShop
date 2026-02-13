dd3e111da8883d057d3391d2ddc144aeeeda9f0b
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-02-13 18:23:04 +0300

    feat: implement basket microservice with Redis integration

---
+++ Services/Basket/MultiShop.Basket/LoginServices/LoginService.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Claims;
+
+namespace MultiShop.Basket.LoginServices
+{
+    public class LoginService : ILoginService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LoginService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public string GetUserId => _httpContextAccessor.HttpContext.User.Claims.FirstOrDefault(x => x.Type == "sub")?.Value;
+     
+
+    }
+}
