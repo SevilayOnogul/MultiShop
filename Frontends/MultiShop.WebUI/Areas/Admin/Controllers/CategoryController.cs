1baba6d65bccf16898a6813244c4bf421d65577a
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-02-15 23:22:06 +0300

    feat(admin): implement admin dashboard theme (part 2)

--- Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -8,6 +8,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
 {
     [Area("Admin")]
     [AllowAnonymous]
+    [Route("Admin/Category")]
 
     public class CategoryController : Controller
     {
@@ -17,7 +18,7 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         {
             _httpClientFactory = httpClientFactory;
         }
-
+        [Route("Index")]
         public async Task<IActionResult> Index()
         {
             ViewBag.v1 = "Ana Sayfa";
@@ -38,6 +39,8 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
         }
 
         [HttpGet]
+        [Route("CreateCategory")]
+
         public IActionResult CreateCategory()
         {
             ViewBag.v1 = "Ana Sayfa";
@@ -47,6 +50,8 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             return View();
         }  
         [HttpPost]
+        [Route("CreateCategory")]
+
         public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
         {
             var client=_httpClientFactory.CreateClient();
@@ -54,6 +59,53 @@ namespace MultiShop.WebUI.Areas.Admin.Controllers
             StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
             var responseMessage = await client.PostAsync("http://localhost:7070/api/Categories",stringContent);
             if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("DeleteCategory/{id}")]
+
+        public async Task<IActionResult>DeleteCategory(string id)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage= await client.DeleteAsync("http://localhost:7070/api/Categories?id=" + id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index", "Category", new { area = "Admin" });
+            }
+            return View();
+        }
+
+        [Route("UpdateCategory/{id}")]
+        [HttpGet]
+        public async Task<IActionResult>UpdateCategory(string id)
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Güncelleme Sayfası";
+            ViewBag.v0 = "Kategori İşlemleri";
+            var client=_httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7070/api/Categories/" + id);
+            if(responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData=await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateCategoryDto>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+
+        [Route("UpdateCategory/{id}")]
+        [HttpPost]
+        public async Task<IActionResult>UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData=JsonConvert.SerializeObject(updateCategoryDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PutAsync("http://localhost:7070/api/Categories", stringContent);
+            if(responseMessage.IsSuccessStatusCode)
             {
                 return RedirectToAction("Index", "Category", new { area = "Admin" });
             }
