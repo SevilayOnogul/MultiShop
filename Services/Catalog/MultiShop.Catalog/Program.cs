c71456a89f85d5855730f610ed1c702691d020ef
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-02-18 01:58:58 +0300

    feat(about): implement complete about feature (admin, API, frontend)

--- Services/Catalog/MultiShop.Catalog/Program.cs
+++ Services/Catalog/MultiShop.Catalog/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using MultiShop.Catalog.Mapping;
+using MultiShop.Catalog.Services.AboutServices;
 using MultiShop.Catalog.Services.BrandServices;
 using MultiShop.Catalog.Services.CategoryServices;
 using MultiShop.Catalog.Services.FeatureServices;
@@ -33,6 +34,7 @@ builder.Services.AddScoped<ISpecialOfferService, SpecialOfferService>();
 builder.Services.AddScoped<IFeatureService, FeatureService>();
 builder.Services.AddScoped<IOfferDiscountService, OfferDiscountService>();
 builder.Services.AddScoped<IBrandService, BrandService>();
+builder.Services.AddScoped<IAboutService,AboutService>();
 
 
 //builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
