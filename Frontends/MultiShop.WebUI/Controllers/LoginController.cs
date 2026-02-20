0f4ae229573b5c32b2934611646c77a5de39c15b
SevilayOnogul <sevilayonogul34@hotmail.com>
2026-02-21 00:12:05 +0300

    feat(identity): implement manual JWT token generation

--- Frontends/MultiShop.WebUI/Controllers/LoginController.cs
+++ Frontends/MultiShop.WebUI/Controllers/LoginController.cs
@@ -1,12 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.IdentityDtos.LoginDtos;
+using MultiShop.WebUI.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
 
 namespace MultiShop.WebUI.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult>Index(CreateLoginDto createLoginDto)
+        {
+            var client=_httpClientFactory.CreateClient();
+            var content = new StringContent(JsonSerializer.Serialize(createLoginDto), Encoding.UTF8, "application/json");
+            var response = await client.PostAsync("http://localhost:5001/api/Logins", content);
+            if(response.IsSuccessStatusCode)
+            {
+                var jsonData=await response.Content.ReadAsStringAsync();
+                var tokenModel = JsonSerializer.Deserialize<JwtResponseModel>(jsonData, new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+                if(tokenModel != null)
+                {
+                    JwtSecurityTokenHandler handler= new JwtSecurityTokenHandler();// JWT token'ları okuyup parçalayabilen bir kütüphane başlatıyoruz.
+                    var token = handler.ReadJwtToken(tokenModel.Token);// "tokenModel.Token" içindeki o uzun şifreli metni (Header.Payload.Signature) açıyoruz.
+                    var claims =token.Claims.ToList();// Token'ın içinden kullanıcı ID, Rol vb. bilgileri (Claims) bir liste olarak çıkarıyoruz.
+
+                    if (tokenModel.Token!=null)
+                    {
+                        claims.Add(new Claim("multishoptoken",tokenModel.Token));
+                        var claimsIdentity=new ClaimsIdentity(claims,JwtBearerDefaults.AuthenticationScheme);
+                        var authProps = new AuthenticationProperties
+                        {
+                            ExpiresUtc = tokenModel.ExpireDate,
+                            IsPersistent = true,
+                        };
+
+                        await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme,new ClaimsPrincipal(claimsIdentity),authProps);
+                        return RedirectToAction("Index", "Default");
+                    }
+                }
+            }
+            return View();
+
         }
     }
 }
--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -1,5 +1,20 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using MultiShop.WebUI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddCookie(JwtBearerDefaults.AuthenticationScheme, opt =>
+{
+    opt.LoginPath = "/Login/Index/";
+    opt.LogoutPath = "/Login/LogOut/";
+    opt.AccessDeniedPath = "/Pages/AccessDanied";
+    opt.Cookie.HttpOnly = true;
+    opt.Cookie.SameSite=SameSiteMode.Strict;
+    opt.Cookie.SecurePolicy=CookieSecurePolicy.SameAsRequest;
+    opt.Cookie.Name = "MultiShopJwt";
+});
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<ILoginService, LoginService>();
 // Add services to the container.
 builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
@@ -18,7 +33,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
